4b15ed061d7fddd338a10f7dfb1e9d383ffeabe4
zacr0 <zacro.suarez@gmail.com>
2018-12-03 10:57:53 +0100

    Added code to plot trajectories from 5-frame snippets to compare between GT poses and predicted poses by PoseNet.

--- plot_trajectories.py
+++ plot_trajectories.py
@@ -1,7 +1,9 @@
 import argparse
 import glob
+import os
 import numpy as np
 from trajectory_utils import merge_sequences_poses
+from kitti_eval.pose_evaluation_utils import read_file_list, associate
 import matplotlib as mpl
 #from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
@@ -22,10 +24,12 @@ def load_gt(gt_dir):
 
     return gt_poses
 
-def load_sequence(sequence_dir):
+def load_sequences(sequence_dir):
     seq_arr = []
 
     gt_seqs_files = sorted(glob.glob(sequence_dir + "*.txt"))
+    # filter out files so only last frame overlaps
+    # gt_seqs_files = [gt_seqs_files[i] for i in range(len(gt_seqs_files)) if i % 4 == 0]
     for file in gt_seqs_files:
         with open(file, 'r') as f:
             lines = f.readlines()
@@ -36,6 +40,49 @@ def load_sequence(sequence_dir):
         seq_arr.append(seq)
 
     return seq_arr
+
+
+def load_scaled_sequences(gt_dir, pred_dir):
+    gt_seqs = []
+    pred_seqs_scaled = []
+
+    pred_files = sorted(glob.glob(pred_dir + '/*.txt'))
+    for i in range(len(pred_files)):
+        gt_file = gt_dir + os.path.basename(pred_files[i])
+
+        # Find matches between predictions and GT poses
+        gt_list = read_file_list(gt_file)
+        pred_list = read_file_list(pred_files[i])
+        matches = associate(gt_list, pred_list, 0, 0.01)
+        if len(matches) < 2:
+            return False
+
+        # Convert to float
+        gt_seq = np.array([[float(val) for val in gt_list[a][:]] for a,b in matches])
+        pred_seq = np.array([[float(val) for val in pred_list[a][:]] for a,b in matches])
+
+        # Align first matched frames
+        offset = gt_seq[0,0:3] - pred_seq[0,0:3]
+        pred_seq[:,0:3] += offset[None,:]
+
+        # Get scaling factor (sx, sy, sz)
+        sx = np.sum(gt_seq[:,0] * pred_seq[:,0]) / np.sum(pred_seq[:,0] ** 2)
+        sy = np.sum(gt_seq[:,1] * pred_seq[:,1]) / np.sum(pred_seq[:,1] ** 2)
+        sz = np.sum(gt_seq[:,2] * pred_seq[:,2]) / np.sum(pred_seq[:,2] ** 2)
+
+        # Scale predictions to GT scale
+        scaled_xs = pred_seq[:,0] * sx
+        scaled_ys = pred_seq[:,1] * sy
+        scaled_zs = pred_seq[:,2] * sz
+        pred_seq_scaled = pred_seq
+        pred_seq_scaled[:,0:3] = np.array([scaled_xs, scaled_ys, scaled_zs]).transpose()
+
+        # Add to list of scaled predictions
+        gt_seqs.append(gt_seq)
+        pred_seqs_scaled.append(pred_seq)
+
+    return gt_seqs, pred_seqs_scaled
+
 
 
 def plot_trajectories(gt_points, pred_points):
@@ -53,18 +100,28 @@ def plot_trajectories(gt_points, pred_points):
 
 
 def main():
-    # load sequences
-    #poses_gt = load_sequence(args.gtruth_dir)
-    poses_gt = load_gt(args.gtruth_dir)
-    poses_pred = load_sequence(args.pred_dir)
+    # # load sequences
+    # poses_gt = load_sequences(args.gtruth_dir)
+    # #poses_gt = load_gt(args.gtruth_dir)
+    # poses_pred = load_sequences(args.pred_dir)
+    #
+    # # merge poses
+    # gt_x, gt_y, gt_z = merge_sequences_poses(poses_gt)
+    # merged_gt = np.array([gt_x, gt_y, gt_z]).transpose()
+    # #merged_gt = poses_gt[:,0:3]
+    # pred_x, pred_y, pred_z = merge_sequences_poses(poses_pred)
+    # merged_pred = np.array([pred_x, pred_y, pred_z]).transpose()
 
-    # merge poses
-    #gt_x, gt_y, gt_z = merge_sequences_poses(poses_gt)
-    #merged_gt = np.array([gt_x, gt_y, gt_z]).transpose()
-    merged_gt = poses_gt[:,0:3]
+    # Load and scale 5 frame sequences
+    poses_gt, poses_pred = load_scaled_sequences(args.gtruth_dir, args.pred_dir)
+
+    # Merge into a single trajectory by pose composition
+    gt_x, gt_y, gt_z = merge_sequences_poses(poses_gt)
+    merged_gt = np.array([gt_x, gt_y, gt_z]).transpose()
     pred_x, pred_y, pred_z = merge_sequences_poses(poses_pred)
     merged_pred = np.array([pred_x, pred_y, pred_z]).transpose()
 
+    # Draw trajectories
     plot_trajectories(merged_gt, merged_pred)
 
 
--- trajectory_utils.py
+++ trajectory_utils.py
@@ -2,6 +2,16 @@ import numpy as np
 from kitti_eval.pose_evaluation_utils import pose_vec_to_mat
 
 
+def convert_sequence_to_mat(poses):
+    poses_mat = []
+
+    for pose_vec in poses:
+        pose_mat = pose_vec_to_mat(pose_vec)
+        poses_mat.append(pose_mat)
+
+    return poses_mat
+
+
 def convert_and_change_coordinate_system(poses, new_coord_index=0):
     coord_pose = pose_vec_to_mat(poses[new_coord_index])
 
@@ -15,32 +25,33 @@ def convert_and_change_coordinate_system(poses, new_coord_index=0):
 
 
 # ps - sequence of N poses (predicted pose vector from network output, e.g. N=5)
-# ps_arr - array of poses sequences with a single overlapping pose (last element)
+# ps_arr - array of poses sequences
 def merge_sequences_poses(ps_arr):
-    ps_arr = [convert_and_change_coordinate_system(ps) for ps in ps_arr]
+    #ps_arr = [convert_and_change_coordinate_system(ps) for ps in ps_arr]
+    ps_arr = [convert_sequence_to_mat(ps) for ps in ps_arr]
 
     poses_global = []
     first_seq = True
-    id_global = 0
-
+    idx_global = 0
     for ps in ps_arr:
         if first_seq:
-            ps_ = ps
+            # First sequence is already in global coordinates, take p0 and p1
+            ps_ = ps[0:2]
             first_seq = False
         else:
-            # TODO: code
             ps_ = []
-            p = ps[-1]
-            p_ = np.dot(poses_global[id_global], p)
+            p = ps[1]
+            p_ = np.dot(poses_global[idx_global], p)
             ps_.append(p_)
-        
-        id_global = id_global+1
 
-        # Add to global poses
+        idx_global += 1
+
+        # Add transformed pose to global poses
         for pose_global in ps_:
             poses_global.append(pose_global)
 
     # convert to sequence
+    print('Tam secuencia: %d ' % len(poses_global))
     poses_global = np.stack(poses_global)
     # get interesting values
     txs = poses_global[:, 0, 3]
@@ -48,29 +59,3 @@ def merge_sequences_poses(ps_arr):
     tzs = poses_global[:, 2, 3]
 
     return txs, tys, tzs
-
-
-    # poses_global = []
-    # ps_prev_last = None
-    # for ps in ps_arr:
-    #     if ps_prev_last is None: # first group - do nothing
-    #         ps_ = ps
-    #     else: # use overlapping pose to translate current ps to global coordinate system
-    #         ps_ = []
-    #         for p in ps:
-    #             p_ = np.dot(ps_prev_last, p)
-    #             ps_.append(p_)
-    #
-    #     ps_prev_last = ps_[-1]
-    #
-    #     # skip the last overlapping pose
-    #     for pose_global in ps_[:-1]:
-    #         poses_global.append(pose_global)
-    #
-    # # get interesting values
-    # poses_stacked = np.stack(poses_global)
-    # txs = poses_stacked[:, 0, 3]
-    # tys = poses_stacked[:, 1, 3]
-    # tzs = poses_stacked[:, 2, 3]
-    #
-    # return txs, tys, tzs # example - outputing just the position (x,y,z)
